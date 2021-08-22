59479f81d791aa8e7a76a073c91bb86951421923
OmerPasa3328 <omerfa33@gmail.com>
2021-08-22 23:52:35 +0300

    yeni düşman eklenmeye calıştı

--- ChaosMushroom/Assets/Scripts/DistanceMovement.cs
+++ ChaosMushroom/Assets/Scripts/DistanceMovement.cs
@@ -1,26 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
 public class DistanceMovement : MonoBehaviour
 {
     public GameObject main;
     public float distance = 0;
-    int chaos;
+    //int chaos;
 
     // Start is called before the first frame update
     void Start()
     {
-        float distance = chaos;
+        CHAOS.chaosValue == distance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distances = Vector3.Distance (main.transform.position, transform.position);
-        if (CHAOS.chaosValue - distance >= 0)
+        float distance = Vector3.Distance (main.transform.position, transform.position);  // kendisiyle ve ana obje arasındaki fark hesaplanır
+        
+        Console.WriteLine(distance);
+        
+        /*if (CHAOS.chaosValue - distance >= 0) //artan chaos puanı 
         {
         CHAOS.chaosValue - distance += chaos;
-        }
+        }*/
     }
 }
