7049f71a1afa4cf48e8b80d025e0fbd034feb0e6
OmerPasa3328 <omerfa33@gmail.com>
2021-08-22 17:08:01 +0300

    further code for chaos score and distance movement

--- ChaosMushroom/Assets/Scripts/DistanceMovement.cs
+++ ChaosMushroom/Assets/Scripts/DistanceMovement.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class DistanceMovement : MonoBehaviour
 {
+    public int chaos = 0;
+    public GameObject main;
+    public float distance = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float distance = chaos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance (object1.transform.position, object2.transform.position);
-
+        float distances = Vector3.Distance (main.transform.position, object2.transform.position);
+        if (chaos - distance >= 0)
+        {
+            chaos - distance += distances;
+        }
     }
 }
