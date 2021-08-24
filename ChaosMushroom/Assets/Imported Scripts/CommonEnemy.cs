5f863c2d5889d8dde7ada7c996f4176556f5357e
OmerPasa3328 <omerfa33@gmail.com>
2021-08-24 15:47:04 +0300

    old scripts new home

---
+++ ChaosMushroom/Assets/Imported Scripts/CommonEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommonEnemy : MonoBehaviour
+{
+    
+    public int health = 100;
+    public int count = 6;
+    public GameObject deathEffect;
+    // Start is called before the first frame update
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+        if (health <=0)
+        {
+            Die();
+        }
+
+    }
+    void Start () 
+    {
+        while (count >= 0)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+            count -= count;
+        }
+    }
+    void Die ()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ ChaosMushroom/Assets/Imported Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Animator mAnimator = null;
+    public Transform firePoint;
+    public GameObject BulletPrefab;
+    
+    //IEnumerator waiter(){yield return new WaitForSeconds(2);}
+    //System. Threading. Thread. Sleep(100);
+   
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(BulletPrefab,firePoint.position,firePoint.rotation);
+            if(mAnimator.gameObject.activeSelf)
+            {
+            mAnimator.SetTrigger("Shoot");
+            }
+            //GameObject tmp = Instantiate<GameObject>(BulletPrefab);
+        }
+    }
+    //void Shoot()
+    //{
+    //    Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+    //}
+    //IEnumerator waiter(){}
+}
