0b0f0b906b36e9379337d530e58139bad37732e4
OmerPasa3328 <omerfa33@gmail.com>
2021-08-24 23:24:40 +0300

    just made a bit cleaning  and sorting

---
+++ ChaosMushroom/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform target;
+    public Transform EnemyTurtleGF;
+    public float speed = 50f;
+    public float NextWayPointDistance = 3f;
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+
+    Seeker seeker;
+    Rigidbody2D rb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Seeker seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("UpdatePath", 0f, .5f);
+    }void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        } else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+        rb.AddForce(force);
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < NextWayPointDistance)
+        {
+            currentWaypoint++;
+            
+        }
+        if (rb.velocity.x >= 0.01f)
+        {
+            EnemyTurtleGF.localScale = new Vector3(-1f, 1f, 1f);
+        } else if (rb.velocity.x <= -0.01f)
+        {
+            EnemyTurtleGF.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+}
