01114dfb23c07f692e6dde42c6aebcd81e14228f
OmerPasa3328 <omerfa33@gmail.com>
2021-08-25 14:31:09 +0300

    com

--- ChaosMushroom/Assets/Scripts/EnemyGFX.cs
+++ ChaosMushroom/Assets/Scripts/EnemyGFX.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyGFX : MonoBehaviour
 {
+    [SerializeField]
     public AIPath aiPath;
     // Start is called before the first frame update
     void Start()
