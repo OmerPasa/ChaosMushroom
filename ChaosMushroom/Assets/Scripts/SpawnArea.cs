ea37c52fbdb743f7a43562f6fcf170826e3e475b
OmerPasa3328 <omerfa33@gmail.com>
2021-08-25 16:29:17 +0300

    mermi ekleniyor

--- ChaosMushroom/Assets/Scripts/SpawnArea.cs
+++ ChaosMushroom/Assets/Scripts/SpawnArea.cs
@@ -5,7 +5,7 @@ using System.Timers;
 
 public class SpawnArea : MonoBehaviour
 {
-    public GameObject bug;
+    public GameObject Trutle;
     public int xPos;
     public int yPos;
     public int enemyCount;
@@ -16,15 +16,13 @@ public class SpawnArea : MonoBehaviour
     }
     IEnumerator EnemyDrop()
    {
-       while (enemyCount < 300)
+       while (enemyCount < 10)
        {
            xPos = (int)Random.Range(-1f,1f);
            yPos = (int)Random.Range(8f,-7f);
-           Instantiate(bug,new Vector3(xPos,yPos,-0.1f),Quaternion.identity);
+           Instantiate(Trutle,new Vector3(xPos,yPos,-0.1f),Quaternion.identity);
            yield return new WaitForSeconds(0.1f);
            enemyCount += 1;
-    
-
        }
    }
 
